9d32862b89dcd33c5a3b24aa3def47a3c8c3a28d
Bhavya Hoda <bhavyahoda@microsoft.com>
2023-09-13 12:44:03 +0530

    Made profile page and pushed lastest code

--- app/src/main/java/com/example/asara/MainActivity.java
+++ app/src/main/java/com/example/asara/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.asara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        my_profile = findViewById(R.id.my_profile);
+        my_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/asara/Profile.java
+++ app/src/main/java/com/example/asara/Profile.java
@@ -1,9 +1,17 @@
 package com.example.asara;
 
+import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
-    Button
+    Button Login, Register;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
 }
