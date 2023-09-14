454ca13b5e568fd24c7b555146e669ebb550d044
kannupriyaRana <kannupriyarana7531@gmail.com>
2023-09-14 17:40:37 +0530

    loginRegisterActivity ready & connect loginRegisterActivity to VetRegistrationActivity

---
+++ app/src/main/java/com/example/asara/LoginRegisterActivity.java
@@ -0,0 +1,46 @@
+package com.example.asara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+        ImageView loginImageView = findViewById(R.id.loginImageView);
+        ImageView registerAsVolunteer = findViewById(R.id.registerAsVolunteer);
+        ImageView registerAsOrg = findViewById(R.id.registerAsOrg);
+        loginImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginRegisterActivity.this, "Logging in", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        registerAsOrg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegisterActivity.this, VetRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerAsVolunteer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginRegisterActivity.this, "Registering as volunteer", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+    }
+    
+}
--- app/src/main/java/com/example/asara/Profile.java
+++ app/src/main/java/com/example/asara/Profile.java
@@ -12,23 +12,23 @@ public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        Login = findViewById(R.id.Login);
-        Register = findViewById(R.id.Register);
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-            }
-        });
-        Register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(getApplicationContext(), Register.class);
-                startActivity(intent);
-            }
-        });
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_profile);
+//        Login = findViewById(R.id.Login);
+//        Register = findViewById(R.id.Register);
+//        Login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent= new Intent(getApplicationContext(), Login.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent= new Intent(getApplicationContext(), Register.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
