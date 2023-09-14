7d386ce3a494a18263263ebdf2845defa5d19b5f
Bhavya Hoda <bhavyahoda@microsoft.com>
2023-09-14 10:01:59 +0530

    Some more pages made

--- app/src/main/java/com/example/asara/Profile.java
+++ app/src/main/java/com/example/asara/Profile.java
@@ -1,17 +1,33 @@
 package com.example.asara;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class Profile extends AppCompatActivity {
-    Button Login, Register;
+    private Button Login, Register;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Login = findViewById(R.id.Login);
+        Register = findViewById(R.id.Register);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
