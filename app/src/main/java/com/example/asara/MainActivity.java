359b5552d219abc4cad6c9c2574ff9e35cbacda0
AmithshaS <amithsha92@gmail.com>
2023-09-14 11:37:52 +0530

    Mainactivity

--- app/src/main/java/com/example/asara/MainActivity.java
+++ app/src/main/java/com/example/asara/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,14 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         my_profile = findViewById(R.id.my_profile);
+        case_history = findViewById(R.id.case_history);
+        nearest_vet_facility = findViewById(R.id.nearest_vet_facility);
+
+
         my_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent= new Intent(getApplicationContext(), Profile.class);
                 startActivity(intent);
             }
         });
+        case_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CaseHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nearest_vet_facility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NearestVetFacilityActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
