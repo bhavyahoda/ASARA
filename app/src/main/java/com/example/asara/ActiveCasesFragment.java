4bd2e7b0b22d2e80faf51858ab9384da2600c6c1
kannupriyaRana <kannupriyarana7531@gmail.com>
2023-09-17 15:58:10 +0530

    rating screen complete

--- app/src/main/java/com/example/asara/ActiveCasesFragment.java
+++ app/src/main/java/com/example/asara/ActiveCasesFragment.java
@@ -1,7 +1,9 @@
 package com.example.asara;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,6 +16,9 @@ public class ActiveCasesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_active_cases, container, false);
+        View view =  inflater.inflate(R.layout.fragment_active_cases, container, false);
+        CardView caseID3123 = view.findViewById(R.id.caseID3123);
+
+        return view;
     }
 }
--- app/src/main/java/com/example/asara/ResolvedCasesFragment.java
+++ app/src/main/java/com/example/asara/ResolvedCasesFragment.java
@@ -1,46 +1,34 @@
 package com.example.asara;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ResolvedCasesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ResolvedCasesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ResolvedCasesFragment() {
         // Required empty public constructor
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resolved_cases, container, false);
+        View view = inflater.inflate(R.layout.fragment_resolved_cases, container, false);
+        CardView caseID1333 = view.findViewById(R.id.caseID1333);
+
+        caseID1333.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CaseHistoryInDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
